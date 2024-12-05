bc24f2889cfb12b6b20d826963aab8b808595eec
Nousad Ali <121709928+NausadAli@users.noreply.github.com>
2024-12-05 08:32:01 +0000

    Event Handler Done

--- day_1/src/App.jsx
+++ day_1/src/App.jsx
@@ -35,21 +35,57 @@ function App(){
       <section style={{backgroundColor : 'green', padding: '2rem', color: 'white'}}>Helllo 
         <FaCartArrowDown/>
       </section>
+
+      <Button>HELLO</Button>
+      <Copy />
+
+      <Move />
     </>
   )
 }
 
+const Move = () =>{
 
-function User(props){
-  return(
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.college}</h1>
-      <h1>{props.year}</h1>
+  function moveHandler(){
+    alert('Mouse Move Event Activated')
+    console.log('Mouse mOve....')
+  }
 
-    </div>
+  return (
+    <p onMouseMove = {moveHandler}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam expedita assumenda similique blanditiis quos fugit iste, eos exercitationem voluptas sed.
+    </p>
   )
 }
+
+const Copy = () =>{
+  const handleCopy = ()=>{
+    console.log("Stop Stealing My Content")
+  }
+
+  return <p onCopy={handleCopy}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ut!
+  </p>
+}
+
+
+const Button = ()=>{
+  const handleClick = ()=>{
+    console.log(Math.floor(Math.random()*10+1))
+  }
+  return <button onClick={handleClick}>HELLO</button>
+}
+
+// function User(props){
+//   return(
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h1>{props.age}</h1>
+//       <h1>{props.college}</h1>
+//       <h1>{props.year}</h1>
+
+//     </div>
+//   )
+// }
 
 export default App; 
